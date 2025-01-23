1dc9b15f111585b6b5a8b0a20d906e969b8fa3ab
xpanfy00 <y.panfyorov@gmail.com>
2025-01-23 21:48:26 +0100

    Edit variables in application.properties

--- src/main/java/com/carter/tgbot/TgbotApplication.java
+++ src/main/java/com/carter/tgbot/TgbotApplication.java
@@ -9,6 +9,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TgbotApplication {
 
     public static void main(String[] args) {
+        System.out.println("PGHOST: " + System.getenv("PGHOST"));
+        System.out.println("PGPORT: " + System.getenv("PGPORT"));
+        System.out.println("PGDATABASE: " + System.getenv("PGDATABASE"));
+        System.out.println("POSTGRES_USER: " + System.getenv("POSTGRES_USER"));
+        System.out.println("POSTGRES_PASSWORD: " + System.getenv("POSTGRES_PASSWORD"));
         SpringApplication.run(TgbotApplication.class, args);
     }
 
